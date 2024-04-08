49a4bc97fb76d91f36c677d2ad56b9b4f663c264
kaminskasandra <san.porombka@gmail.com>
2024-04-08 10:20:48 +0200

    JDP240401-2_GroupsController - initial implementation

---
+++ src/main/java/com/kodilla/ecommercee/domain/dto/GroupDto.java
@@ -0,0 +1,11 @@
+package com.kodilla.ecommercee.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GroupDto {
+    private int groupId;
+    private String description;
+}
