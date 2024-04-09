f7a294fe51debb0ac8f5975745fd53f432a2f46b
Chojnacki <twojemail@example.com>
2024-04-09 21:41:51 +0200

    JDP240401-14 Encja Cart - implementacja

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -2,13 +2,35 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.antlr.v4.runtime.misc.NotNull;
 
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.util.List;
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "CARTS")
 public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CART_ID")
     private int id;
-    private int userId;
-    private int cartId;
-    private List<String> products;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @NotNull
+    private LocalDate created;
+
+    @ManyToMany(cascade = {CascadeType.ALL})
+    @JoinTable(
+            name = "JOIN_CART_PRODUCT",
+            joinColumns = @JoinColumn(name = "CART_ID"),
+            inverseJoinColumns = @JoinColumn(name = "PRODUCT_ID"))
+    private List<Product> products;
+
 }
