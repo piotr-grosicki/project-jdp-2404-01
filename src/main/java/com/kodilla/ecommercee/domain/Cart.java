447ba8aea7dd2d63dc4ba2e9f44236fc123bf856
Tomasz Szczygiel <szczgl@wp.pl>
2024-04-10 21:23:04 +0200

    s

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -1,6 +1,5 @@
 package com.kodilla.ecommercee.domain;
 
-<<<<<<< HEAD
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -12,7 +11,4 @@ public class Cart {
     private int userId;
     private int cartId;
     private List<String> products;
-=======
-public class Cart {
->>>>>>> 30c4fcb (Add Encje User JDP240401-15)
 }
