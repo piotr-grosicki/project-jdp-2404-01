5004efa4f7686eef060fceed8b71e40f8192e2cc
Chojnacki <twojemail@example.com>
2024-04-12 19:52:48 +0200

    Dodanie CartRepository

---
+++ src/main/java/com/kodilla/ecommercee/repository/CartRepository.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Cart;
+import jakarta.transaction.Transactional;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Transactional
+@Repository
+public interface CartRepository extends CrudRepository<Cart, Long> {
+
+}
