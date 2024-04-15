e994e921fbae6f595064eb4540673f61559aeec3
adrtre <adriantretyn@gmail.com>
2024-04-15 20:00:22 +0200

    Dodano testy dla klasy Group

--- src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/ProductRepository.java
@@ -1,14 +1,21 @@
 package com.kodilla.ecommercee.repository;
 
+import com.kodilla.ecommercee.domain.Group;
 import com.kodilla.ecommercee.domain.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
     @Override
     List<Product> findAll();
+
+    void deleteAllByGroup(Group savedGroup);
+
+    Optional<Object> findAllByGroup(Group savedGroup);
 }
 
 
